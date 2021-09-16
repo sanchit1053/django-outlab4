ef7f8213b70e789facb6be6dea325c15d8f0a9b9
sanchit1053 <sanchit1053@gmail.com>
2021-09-16 23:40:59 +0530

    timeone final4

--- myproject/myprofile/views.py
+++ myproject/myprofile/views.py
@@ -19,15 +19,13 @@ def UpdateView(request,username):
     response = requests.get(f" https://api.github.com/users/{username}")
     response = response.json()
     f = "%Y-%m-%dT%H:%M:%SZ"
-    tz = pytz.timezone('Asia/Kolkata')
+    #tz = pytz.timezone('Asia/Kolkata')
     if response.get('followers') != None:
         user.profiles.numOfFollowers = response['followers']
         #now = datetime.now()
         #now = now.strftime("%b %-d,%Y %-I:%M%p")
-        now = datetime.now(tz)
-        #now.setHours(now.getHours()+ 5)
-        #now.setMinutes(now.getMinutes()+30)
-        #now = now + timedelta(hours = 5, minutes = 30)
+        now = datetime.now(tz = timezone.utc)
+        now = now + timedelta(hours = 5, minutes = 30)
         user.profiles.lastUpdated = now
         user.profiles.save()
         #user.profiles.lastUpdated = datetime.strptime(response['updated_at'] , f) 
